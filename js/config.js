2bbaa48f1574e6223ede0195c7a82aaff808348d
Thibault Jacquet <jacquett@ext.essilor.com>
2020-10-19 17:31:55 +0200

    feat: add secured api call

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://5u4vlcm633.execute-api.eu-west-3.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
